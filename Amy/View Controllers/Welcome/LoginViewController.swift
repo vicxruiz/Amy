f4bb499b30732759de9f6e6e091f97ea8ba8b407
victor <ruizdvictor@gmail.com>
2019-06-27 11:33:32 -0400

    added data getter for fetching data

---
+++ Amy/View Controllers/Welcome/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  Amy
+//
+//  Created by Victor  on 6/25/19.
+//  Copyright © 2019 Victor . All rights reserved.
+//
+
+import Foundation
---
+++ Amy/View Controllers/Welcome/SignUpViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SignUpViewController.swift
+//  Amy
+//
+//  Created by Victor  on 6/25/19.
+//  Copyright © 2019 Victor . All rights reserved.
+//
+
+import Foundation
