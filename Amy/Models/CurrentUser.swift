7c63b7902e32084d5a94bab73fc2482239bc6b8a
victor <ruizdvictor@gmail.com>
2019-06-27 11:36:13 -0400

    updated current user object

--- Amy/Models/CurrentUser.swift
+++ Amy/Models/CurrentUser.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+import UIKit
+
+//Model for user signed in
+struct CurrentUser {
+    let uid: String
+    var name: String
+    var phoneNumber: String
+    var email: String
+    
+    init(uid: String, dictionary: [String: Any]) {
+        self.uid = uid
+        self.name = dictionary["name"] as? String ?? ""
+        self.phoneNumber = dictionary["phone number"] as? String ?? ""
+        self.email = dictionary["email"] as? String ?? ""
+    }
+}
